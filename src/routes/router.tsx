6378239ead117a08920539cd9adac4d4145ccd45
Valentine Efagene <33757347+Valentine-Efagene@users.noreply.github.com>
2023-05-24 23:00:17 +0100

    Fix: Fix routing

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import Applications from "../pages/Applications/Applications";
 import App from "../App";
 import NotFound from "../pages/NotFound/NotFound";
@@ -7,7 +7,7 @@ import ModalViewer from "../pages/ModalViewer/ModalViewer";
 const router = createBrowserRouter([
   {
     path: "",
-    element: <Applications />,
+    element: <Navigate to="/investment-management" />,
   },
   {
     path: "/modals",
@@ -17,10 +17,6 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      {
-        path: "/",
-        element: <Applications />,
-      },
       {
         path: "/investment-management",
         element: <Applications />,
